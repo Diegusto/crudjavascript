c6d8063900a7720a50a02c54a0acc65466dffbd9
Diegusto <diegusto35@gmail.com>
2022-08-25 18:51:34 -0300

    feat[crud]: adição do update e do delete

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 
 const express = require('express')
+const { read } = require('fs')
+const { type } = require('os')
+const { rawListeners } = require('process')
 const app = express()
 app.use(express.json())
 const port = 3000
 const tarefas = []
-
+const readline = require('readline')
 
 //exibe a lista de tarefas
 app.get('/tarefas', (req, res) => 
@@ -15,8 +18,9 @@ app.get('/tarefas', (req, res) =>
 //cria tarefas
 app.post ('/tarefas', (req, res) =>
 {
-    const tarefa = req.body
+    const {tarefa} = req.body
     tarefas.push(tarefa)
+    tarefas.push("não concluido")
 
     return res.json(tarefas)
 })
@@ -25,3 +29,64 @@ app.listen(port, () => {
   console.log(`listening on port ${port}`)
 })
 
+//atualiza tarefas
+app.put ('/tarefas/:index', (req, res) =>
+{
+  const {index} = req.params
+  const {tarefa}= req.body
+
+  tarefas[index] = tarefa
+
+  return res.json(tarefas)
+})
+
+
+//conclui tarefas
+const pergunta = readline.createInterface
+({
+  input: process.stdin,
+  output: process.stout
+})
+
+console.log("pressione 1 para concluir uma tarefa")
+pergunta.question("?", function concluir (resposta)
+{
+  if (resposta == "1")
+  {
+    console.log(tarefas)
+
+    console.log("escolha uma tarefa para concluir")
+    pergunta.close()
+
+    const pergunta1 = readline.createInterface
+    ({
+      input: process.stdin,
+      output: process.stdout
+    })
+
+    pergunta1.question(":", function alterar (resposta1)
+    {
+        let tarefaint = parseInt(resposta1)
+        if (tarefaint != 1)
+        {
+          tarefaint++
+        }
+  
+        tarefas.splice(tarefaint, 1, "concluido")
+        console.log(tarefaint)
+        console.log(tarefas)
+        pergunta.close()
+  }
+  )
+  }
+}
+)
+
+
+app.delete('/tarefas/:index', (req, res) => {
+  const {index} = req.params
+
+  tarefas.splice(index, 2)
+
+  return res.send()
+})
